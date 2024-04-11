1346519d0ce3791ff1c7d28b225da8aeb9dc49fb
Nitin Lodhi <nlnitinlodhi@gmail.com>
2024-04-11 06:00:15 +0530

    string support part-1

---
+++ clox/include/table.h
@@ -0,0 +1,25 @@
+#ifndef __CLOX_TABLE_H__
+#define __CLOX_TABLE_H__
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry *entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
+
+#endif
