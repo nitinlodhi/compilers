75ec3afda666d15fcee3aeb05f58a97cbe156c57
Nitin Lodhi <nlnitinlodhi@gmail.com>
2024-04-16 16:42:14 +0530

    spaces

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -158,46 +158,46 @@ static void literal() {
 }
 
 ParseRule rules[] = {
-  [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
-  [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
-  [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_MINUS]         = {unary,    binary, PREC_TERM},
-  [TOKEN_PLUS]          = {NULL,     binary, PREC_TERM},
-  [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
-  [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
-  [TOKEN_BANG]          = {unary,     NULL,   PREC_NONE},
-  [TOKEN_BANG_EQUAL]    = {NULL,     binary,   PREC_EQUALITY},
-  [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_EQUAL_EQUAL]   = {NULL,     binary,   PREC_EQUALITY},
-  [TOKEN_GREATER]       = {NULL,     binary,   PREC_COMPARISON},
-  [TOKEN_GREATER_EQUAL] = {NULL,     binary,   PREC_COMPARISON},
-  [TOKEN_LESS]          = {NULL,     binary,   PREC_COMPARISON},
-  [TOKEN_LESS_EQUAL]    = {NULL,     binary,   PREC_COMPARISON},
-  [TOKEN_IDENTIFIER]    = {variable,     NULL,   PREC_NONE},
-  [TOKEN_STRING]        = {string,     NULL,   PREC_NONE},
-  [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
-  [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_FALSE]         = {literal,     NULL,   PREC_NONE},
-  [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_NIL]           = {literal,     NULL,   PREC_NONE},
-  [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_TRUE]          = {literal,     NULL,   PREC_NONE},
-  [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LEFT_PAREN]    = {grouping,    NULL,       PREC_NONE},
+  [TOKEN_RIGHT_PAREN]   = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_LEFT_BRACE]    = {NULL,        NULL,       PREC_NONE}, 
+  [TOKEN_RIGHT_BRACE]   = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_COMMA]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_DOT]           = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_MINUS]         = {unary,       binary,     PREC_TERM},
+  [TOKEN_PLUS]          = {NULL,        binary,     PREC_TERM},
+  [TOKEN_SEMICOLON]     = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_SLASH]         = {NULL,        binary,     PREC_FACTOR},
+  [TOKEN_STAR]          = {NULL,        binary,     PREC_FACTOR},
+  [TOKEN_BANG]          = {unary,       NULL,       PREC_NONE},
+  [TOKEN_BANG_EQUAL]    = {NULL,        binary,     PREC_EQUALITY},
+  [TOKEN_EQUAL]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_EQUAL_EQUAL]   = {NULL,        binary,     PREC_EQUALITY},
+  [TOKEN_GREATER]       = {NULL,        binary,     PREC_COMPARISON},
+  [TOKEN_GREATER_EQUAL] = {NULL,        binary,     PREC_COMPARISON},
+  [TOKEN_LESS]          = {NULL,        binary,     PREC_COMPARISON},
+  [TOKEN_LESS_EQUAL]    = {NULL,        binary,     PREC_COMPARISON},
+  [TOKEN_IDENTIFIER]    = {variable,    NULL,       PREC_NONE},
+  [TOKEN_STRING]        = {string,      NULL,       PREC_NONE},
+  [TOKEN_NUMBER]        = {number,      NULL,       PREC_NONE},
+  [TOKEN_AND]           = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_CLASS]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_ELSE]          = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_FALSE]         = {literal,     NULL,       PREC_NONE},
+  [TOKEN_FOR]           = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_FUN]           = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_IF]            = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_NIL]           = {literal,     NULL,       PREC_NONE},
+  [TOKEN_OR]            = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_PRINT]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_RETURN]        = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_SUPER]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_THIS]          = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_TRUE]          = {literal,     NULL,       PREC_NONE},
+  [TOKEN_VAR]           = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_WHILE]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_ERROR]         = {NULL,        NULL,       PREC_NONE},
+  [TOKEN_EOF]           = {NULL,        NULL,       PREC_NONE},
 };
 
 static ParseRule* getRule(TokenType type) {
@@ -225,7 +225,7 @@ static void unary() {
     parsePrecedence(PREC_UNARY);
 
     switch (opType) {
-        case TOKEN_BANG: emitByte(OP_NOT); break;
+        case TOKEN_BANG:  emitByte(OP_NOT);    break;
         case TOKEN_MINUS: emitByte(OP_NEGATE); break;
         default: return; // Unreachable
     }
@@ -249,19 +249,17 @@ static uint8_t parseVariable(const char* errorMessage) {
     return identifierConstant(&parser.previous);
 }
 
-static void parsePrecedence(Precedence precedence) {
+static void  parsePrecedence(Precedence precedence) {
     advance();
     ParseFn prefixRule = getRule(parser.previous.type)->prefix;
-    if (prefixRule == NULL)
-    {
+    if (prefixRule == NULL) {
         error("Expect expression.");
         return;
     }
 
     prefixRule();
 
-    while (precedence <= getRule(parser.current.type)->precedence)
-    {
+    while (precedence <= getRule(parser.current.type)->precedence) {
         advance();
         ParseFn infixRule = getRule(parser.previous.type)->infix;
         infixRule();
@@ -274,16 +272,16 @@ static void binary() {
     parsePrecedence((Precedence)(rule->precedence + 1));
 
     switch(opType) {
-        case TOKEN_BANG_EQUAL:      emitBytes(OP_EQUAL, OP_NOT); break;
-        case TOKEN_EQUAL_EQUAL:     emitByte(OP_EQUAL); break;
-        case TOKEN_GREATER:         emitByte(OP_GREATER); break;
-        case TOKEN_GREATER_EQUAL:   emitBytes(OP_LESS, OP_NOT); break;
-        case TOKEN_LESS:            emitByte(OP_LESS); break;
-        case TOKEN_LESS_EQUAL:      emitBytes(OP_GREATER, OP_NOT); break;
-        case TOKEN_PLUS:            emitByte(OP_ADD); break;
-        case TOKEN_MINUS:           emitByte(OP_SUBTRACT); break;
-        case TOKEN_STAR:            emitByte(OP_MULTIPLY); break;
-        case TOKEN_SLASH:           emitByte(OP_DIVIDE); break;
+        case TOKEN_BANG_EQUAL:      emitBytes(OP_EQUAL, OP_NOT);    break;
+        case TOKEN_EQUAL_EQUAL:     emitByte(OP_EQUAL);             break;
+        case TOKEN_GREATER:         emitByte(OP_GREATER);           break;
+        case TOKEN_GREATER_EQUAL:   emitBytes(OP_LESS, OP_NOT);     break;
+        case TOKEN_LESS:            emitByte(OP_LESS);              break;
+        case TOKEN_LESS_EQUAL:      emitBytes(OP_GREATER, OP_NOT);  break;
+        case TOKEN_PLUS:            emitByte(OP_ADD);               break;
+        case TOKEN_MINUS:           emitByte(OP_SUBTRACT);          break;
+        case TOKEN_STAR:            emitByte(OP_MULTIPLY);          break;
+        case TOKEN_SLASH:           emitByte(OP_DIVIDE);            break;
         default: return; // Unreachable
     }
 }
--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -52,24 +52,24 @@ static TokenType checkKeyword(int start, int length,
 
 static TokenType identifierType() {
     switch (scanner.start[0]) {
-        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'a': return checkKeyword(1, 2, "nd",   TOKEN_AND);
         case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
-        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'e': return checkKeyword(1, 3, "lse",  TOKEN_ELSE);
         case 'f':
             if (scanner.current - scanner.start > 1) {
                 switch (scanner.start[1]) {
                 case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
-                case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
-                case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                case 'o': return checkKeyword(2, 1, "r",   TOKEN_FOR);
+                case 'u': return checkKeyword(2, 1, "n",   TOKEN_FUN);
                 }
             }
             break;
-        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
-        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
-        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
-        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
-        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
-        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 'i': return checkKeyword(1, 1, "f",        TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il",       TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r",        TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint",     TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn",    TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper",     TOKEN_SUPER);
         case 't':
             if (scanner.current - scanner.start > 1) {
                 switch (scanner.start[1]) {
@@ -78,7 +78,7 @@ static TokenType identifierType() {
                 }
             }
             break;
-        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'v': return checkKeyword(1, 2, "ar",   TOKEN_VAR);
         case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
     }
     return TOKEN_IDENTIFIER;
