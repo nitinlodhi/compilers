6e7dcf77d7e4be1e33c35344c94770c44090e547
Nitin Lodhi <nlnitinlodhi@gmail.com>
2024-04-20 09:44:22 +0530

    local scopes

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -144,6 +144,11 @@ static void beginScope() {
 
 static void endScope() {
     current->scopeDepth--;
+    while (current->localCount > 0 &&
+        current->locals[current->localCount - 1].depth > current->scopeDepth) {
+        emitByte(OP_POP);
+        current->localCount--;
+    }
 }
 
 static void expression() {
@@ -267,11 +272,18 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local* local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1)
+                error("Can't read local variable in its own initializer.");
             return i;
         }
     }
 
     return -1;
+}
+
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth =
+        current->scopeDepth;
 }
 
 static void addLocal(Token name) {
@@ -281,7 +293,7 @@ static void addLocal(Token name) {
     }
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -289,16 +301,33 @@ static void declareVariable() {
         return;
 
     Token* name = &parser.previous;
+    for (int i = current->localCount - 1; i >= 0; i--) {
+        Local* local = &current->locals[i];
+        if (local->depth != -1 && local->depth > current->scopeDepth)
+            break;
+        if (identifiersEqual(name, &local->name))
+            error("Already a variable with this name in this scope.");
+    }
     addLocal(*name);
 }
 
 static void namedVariable(Token name, bool canAssign) {
-    uint8_t arg = identifierConstant(&name);
+    uint8_t getOp, setOp;
+    int arg = resolveLocal(current, &name);
+    if (arg != -1) {
+        getOp = OP_GET_LOCAL;
+        setOp = OP_SET_LOCAL;
+    } else {
+        arg = identifierConstant(&name);
+        getOp = OP_GET_GLOBAL;
+        setOp = OP_SET_GLOBAL;
+    }
+    
     if (canAssign && match(TOKEN_EQUAL)) {
         expression();
-        emitBytes(OP_SET_GLOBAL, arg);
+        emitBytes(setOp, arg);
     } else {
-        emitBytes(OP_GET_GLOBAL, arg);
+        emitBytes(getOp, arg);
     }
 }
 
@@ -423,6 +452,7 @@ static void synchronize() {
 
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
     emitBytes(OP_DEFINE_GLOBAL, global);
